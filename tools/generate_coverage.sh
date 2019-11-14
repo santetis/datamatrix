a811f43947539ef0527a40ad2240033b326c5f6b
kevin <segaud.kevin@gmail.com>
2019-11-14 19:18:49 +0100

    fix getting coverage
    Signed-off-by: kevin <segaud.kevin@gmail.com>

---
+++ tools/generate_coverage.sh
@@ -0,0 +1,22 @@
+OBS_PORT=9292
+echo "Collecting coverage on port $OBS_PORT..."
+#   Start test in one VM
+dart \
+    --enable-vm-service=$OBS_PORT \
+    --pause-isolates-on-exit \
+    test/coverage.dart &
+
+# Run the coverage collector to generate the JSON coverage report.
+collect_coverage \
+    --port=$OBS_PORT \
+    --out=coverage/coverage.json \
+    --wait-paused \
+    --resume-isolates
+
+echo "Generating LCOV report..."
+format_coverage \
+    --lcov \
+    --in=coverage/coverage.json \
+    --out=coverage/lcov.info \
+    --packages=.packages \
+    --report-on=lib
