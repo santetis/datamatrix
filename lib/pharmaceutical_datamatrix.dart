22e7eab139fa54c38c6c64dd214fc9aa99448141
kevin <segaud.kevin@gmail.com>
2019-11-15 18:37:20 +0100

    improve decoding cip13
    Signed-off-by: kevin <segaud.kevin@gmail.com>

--- lib/pharmaceutical_datamatrix.dart
+++ lib/pharmaceutical_datamatrix.dart
@@ -1,38 +1,124 @@
 import 'package:meta/meta.dart';
 
 class Cip7 {
-  final String cip7;
+  final String data;
 
-  const Cip7({@required this.cip7});
+  Cip7({@required this.data}) {
+    if (data?.length != 7) {
+      throw ArgumentError.value(data, 'cip7', 'length should be equal to 7');
+    }
+  }
 
-  String toString() => cip7;
+  String toString() => data;
 }
 
-class Cip13 {
+enum PharmaceuticalType {
+  speciality,
+  product,
+}
+
+const _pharmaceuticalTypeMap = {
+  '00': PharmaceuticalType.speciality,
+};
+
+enum PharmaceuticalClassification {
+  allopathy,
+}
+
+const _pharmaceuticalClassificationMap = {
+  '9': PharmaceuticalClassification.allopathy,
+};
+
+int _sumFor(String cip13, {bool even = true}) {
+  int result = 0;
+  for (int i = even ? 0 : 1; i < cip13.length; i += 2) {
+    result += int.tryParse(cip13[i]);
+  }
+  return result;
+}
+
+int _calculateCip13Crc(String data) {
+  final odd = _sumFor(data, even: false) * 3;
+  final even = _sumFor(data);
+  final result = odd + even;
+  return 9 - ((result - 1) % 10);
+}
+
+class PharmaceuticalCip13 {
   final String prefix;
+  final PharmaceuticalType type;
+  final PharmaceuticalClassification classification;
   final Cip7 cip7;
+  final String crc;
 
-  const Cip13({@required this.prefix, @required this.cip7});
+  PharmaceuticalCip13._({
+    @required this.prefix,
+    @required this.type,
+    @required this.classification,
+    @required this.cip7,
+    @required this.crc,
+  }) {
+    if (prefix?.length != 2) {
+      throw ArgumentError.value(
+          prefix, 'prefix', 'length should be equal to 2');
+    } else if (prefix != '34') {
+      throw ArgumentError.value(prefix, 'prefix', 'should be equal to 34');
+    } else if (type == null) {
+      throw ArgumentError.notNull('type');
+    } else if (classification == null) {
+      throw ArgumentError.notNull('classification');
+    }
+  }
 
-  factory Cip13.fromCip13(String cip13) {
-    return Cip13(
-      prefix: cip13.substring(0, 6),
-      cip7: Cip7(cip7: cip13.substring(6)),
+  factory PharmaceuticalCip13.fromString(String cip13) {
+    if (cip13.length != 13) {
+      throw ArgumentError.value(cip13, 'cip13', 'length should be equal to 13');
+    } else if (cip13.substring(2, 4) != '00' && cip13.substring(2, 4) != '01') {
+      throw ArgumentError.value(
+          cip13.substring(2, 4), 'type', 'valid value are 00, 01');
+    } else if (!RegExp(r'\d').hasMatch(cip13.substring(4, 5))) {
+      throw ArgumentError.value(
+          cip13.substring(4, 5), 'classification', 'should be a number');
+    }
+    final data = cip13.substring(0, cip13.length - 1);
+    final crc = _calculateCip13Crc(data);
+    if ('$crc' != cip13.substring(cip13.length - 1)) {
+      throw ArgumentError.value(crc, 'crc', 'is not correct');
+    }
+    return PharmaceuticalCip13._(
+      prefix: cip13.substring(0, 2),
+      type: _pharmaceuticalTypeMap[cip13.substring(2, 4)],
+      classification: _pharmaceuticalClassificationMap[cip13.substring(4, 5)],
+      cip7: Cip7(data: cip13.substring(5, cip13.length - 1)),
+      crc: cip13.substring(cip13.length - 1),
     );
   }
 
-  String toString() => '$prefix$cip7';
+  String toString() {
+    final sb = StringBuffer(prefix);
+    final _type = _pharmaceuticalTypeMap.entries
+        .firstWhere((entry) => entry.value == type, orElse: () => null)
+        ?.key;
+    sb.write(_type);
+    final _classification = _pharmaceuticalClassificationMap.entries
+        .firstWhere((entry) => entry.value == classification,
+            orElse: () => null)
+        ?.key;
+    sb.write(_classification);
+    sb.write('$cip7$crc');
+    return sb.toString();
+  }
 }
 
 class PharmaceuticalDatamatrix {
-  final Cip13 cip13;
-  final DateTime created;
+  final PharmaceuticalCip13 cip13;
+  final DateTime manufactured;
   final DateTime expiration;
   final String lot;
 
-  const PharmaceuticalDatamatrix({
+  PharmaceuticalDatamatrix({
     @required this.cip13,
-    @required this.created,
+    @required this.manufactured,
     @required this.expiration,
     @required this.lot,
   });
@@ -43,23 +129,29 @@ class PharmaceuticalDatamatrix {
       throw ArgumentError.notNull('datamatrix');
     }
     final regExp =
-        RegExp(r'^010((\d{6})\d{7})17(\d{6})(\d{6})?10([a-zA-Z0-9]+)$');
+        RegExp(r'^010(((\d{5})\d{7})\d)17(\d{6})(\d{6})?10([a-zA-Z0-9]+)$');
     if (!regExp.hasMatch(datamatrix)) {
       throw ArgumentError.value(datamatrix, 'datamatrix', 'is not valid');
     }
 
     final match = regExp.firstMatch(datamatrix);
     final cip13 = match.group(1);
-    final created = match.group(3);
-    final expiration = match.group(4);
-    final lot = match.group(5);
+    final created = match.group(4);
+    final expiration = match.group(5);
+    final lot = match.group(6);
 
     return PharmaceuticalDatamatrix(
-      cip13: Cip13.fromCip13(cip13),
-      created: _parseDate(created),
+      cip13: PharmaceuticalCip13.fromString(cip13),
+      manufactured: _parseDate(created),
       expiration: _parseDate(expiration),
       lot: lot,
     );
+  }
+
+  String toString() {
+    final sb = StringBuffer('010');
+
+    return sb.toString();
   }
 }
 
--- test/pharmaceutical_datamatrix_test.dart
+++ test/pharmaceutical_datamatrix_test.dart
@@ -2,16 +2,46 @@ import 'package:pharmaceutical_datamatrix/pharmaceutical_datamatrix.dart';
 import 'package:test/test.dart';
 
 void main() {
+  test('invalid cip7', () {
+    expect(
+      () => Cip7(
+        data: '111111',
+      ),
+      throwsA(isArgumentError),
+    );
+  });
+
+  test('valid cip7', () {
+    final cip7 = Cip7(
+      data: '1111111',
+    );
+
+    expect(cip7.data, '1111111');
+  });
+
+  test('cip13 from invalid String', () {
+    expect(
+      () => PharmaceuticalCip13.fromString('123123'),
+      throwsA(isArgumentError),
+    );
+  });
+
+  test('cip13 from String', () {
+    final cip13 = PharmaceuticalCip13.fromString('3400935510259');
+    expect(cip13.prefix, '34');
+    expect(cip13.cip7.toString(), '3551025');
+  });
+
   test('parse datamatrix data', () {
-    final datamatrixData = '0103400091111111170101120101121012lt';
+    final datamatrixData = '0103400949497294170101120101121012lt';
 
     final pharmaceuticalDatamatrix = PharmaceuticalDatamatrix.fromDatamatrix(
       datamatrix: datamatrixData,
     );
 
-    expect(pharmaceuticalDatamatrix.cip13.toString(), '3400091111111');
+    expect(pharmaceuticalDatamatrix.cip13.toString(), '3400949497294');
     expect(pharmaceuticalDatamatrix.lot, '12lt');
-    expect(pharmaceuticalDatamatrix.created, DateTime(12, 01, 01));
+    expect(pharmaceuticalDatamatrix.manufactured, DateTime(12, 01, 01));
     expect(pharmaceuticalDatamatrix.expiration, DateTime(12, 01, 01));
   });
 
